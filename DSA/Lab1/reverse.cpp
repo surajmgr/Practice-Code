83931e55cf24299d7415b84c1a9c066ef1d664b8
surajmgr <surajpulami8@gmail.com>
2022-07-27 18:01:18 +0545

    DSA#Lab1&2

---
+++ DSA/Lab1/reverse.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, rev = 0;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    while (n != 0)
+    {
+        int temp = n % 10;
+        rev = rev * 10 + temp;
+        n /= 10;
+    }
+    cout << "Reverse: " << rev << endl;
+
+    return 0;
+}
