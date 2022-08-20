cf3ef9f915027116345f1e72e84945f089c714fa
Suraj Pulami <surajpulami8@gmail.com>
2022-08-20 19:33:07 +0545

    circularQueue

--- DSA/6_circularQueue.cpp
+++ DSA/6_circularQueue.cpp
@@ -1,57 +1,80 @@
-#include<iostream>
+#include <iostream>
 // #include<conio.h>
 using namespace std;
 
 int main(){
-	int max=10;
-	int que[max], front, rear, num, caseN;
+	int max = 3;
+	int que[max], front = -1, rear = -1, num, caseN;
 	char q;
-	
+
 	do{
-		cout<<"Press: "<<endl<<"1. Empty Queue"<<endl<<"2. Enqueue"<<endl<<"3. Dequeue"<<endl<<"4. Queue Traversal"<<endl;
-		cin>>caseN;
-		switch(caseN){
-			case 1:
-				front=rear=-1;
-				cout<<"Empty!"<<endl;
-				break;
-			case 2:
-				if(front==rear+1){
-					cout<<"Overflow!"<<endl;
+		cout << "Press: " << endl
+			 << "1. Empty Queue" << endl
+			 << "2. Enqueue" << endl
+			 << "3. Dequeue" << endl
+			 << "4. Queue Traversal" << endl;
+		cin >> caseN;
+		switch (caseN){
+		case 1:
+			front = rear = -1;
+			cout << "Empty!" << endl;
+			break;
+		case 2:
+			if ((front == 0 && rear == max - 1) || (front == rear + 1)){
+				cout << "Overflow!" << endl;
+			}
+			else{
+				cout << "Enter the number to be enqueued: ";
+				cin >> num;
+				if (front == -1 && rear == -1)
+					front = rear = 0;
+				else if (rear == max - 1 && front != 0){
+					rear = 0;
 				}
-				else{
-					if (front == -1)
-						front = 0;
-					cout<<"Enter the number to be enqueued: ";
-					cin>>num;
-					rear = (rear + 1) % max;
-					que[rear] = num;
+				else
+					rear += 1;
+				que[rear] = num;
+			}
+			break;
+		case 3:
+			if (front == -1){
+				cout << "Underflow!" << endl;
+			}
+			else if (front == rear){
+				front = rear = -1;
+				cout << "Empty!" << endl;
+			}
+			else{
+				if (front == max-1)
+					front = 0;
+				else
+					front += 1;
+			}
+			break;
+		case 4:
+			if (front == -1){
+				cout << "Empty!" << endl;
+			}
+			else{
+				cout << "Displaying data: " << endl;
+				if(front <= rear){
+					for (int i = front; i <= rear; i++)
+						cout<< que[i] <<endl;
 				}
-				break;
-			case 3:
-				if(front==rear){
-					front=rear=-1;
-					cout<<"Empty!"<<endl;
+				else
+				{
+					for (int i = front; i < max; i++)
+						cout << que[i] <<endl;
+					for (int i = 0; i <= rear; i++)
+						cout << que[i] <<endl;
 				}
-				else{
-					front = (front + 1) % max;
-				}
-				break;
-			case 4:
-				if (front==rear) {
-					cout << "Empty!" << endl;
-				}
-				else{
-					cout<<"Displaying data: "<<endl;
-					for (int i = front; i != rear; i = (i + 1) % max)
-						cout << que[i]<<endl;
-				}
-				break;
-	}
-	cout<<"Do you want to continue? (y/n): ";
-	cin>>q;
-	}while(q=='y');
-	
+			}
+			break;
+		}
+		cout << "Do you want to continue? (y/n): ";
+		cin >> q;
+	} while (q == 'y');
+
 	// getch();
 	return 0;
 }
