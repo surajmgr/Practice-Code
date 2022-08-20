d210fca3b1544644b51800eab06ee5bb9e12e0b9
Suraj Pulami <surajpulami8@gmail.com>
2022-08-20 09:37:48 +0545

    NM_Lab2

--- NM/Lab2/lagrange.c
+++ NM/Lab2/lagrange.c
@@ -1,35 +1,43 @@
+// Polynomial interpolation using Lagrange's Method
 #include <stdio.h>
 #include <math.h>
 #define MAX 15
 
-int main(){
+int main()
+{
     int n;
-    float x[MAX], f[MAX], xp, fp, p;
-    char q;
+    float x[MAX], f[MAX], fp, lf, sum, xp;
+    char q = 'y';
 
-    printf("\nEnter the total no. of data pairs: ");
+    printf("\nInput the number of data pairs: ");
     scanf("%d", &n);
-    printf("\nEnter x(i) and f(i), (one pair at a time): ");
+    printf("\nInput data pairs x(i) and values f(i) (one set in each line): ");
 
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         scanf("%f %f", &x[i], &f[i]);
     }
 
-    do{
-        printf("\nEnter the point, x at which interpolation is required: ");
+    do
+    {
+        printf("\nInput x at which interpolation is required: ");
         scanf("%f", &xp);
-        fp = 0;
-        for(int i=0; i<n; i++){
-		    p=1;
-		    for(int j=0; j<n; j++){
-			    if(i!=j){
-			       	p = p* (xp - x[j])/(x[i] - x[j]);
-			    }
-		    }
-		  fp = fp + p * f[i];
+        sum = 0.0;
+        for (int i = 0; i < n; i++)
+        {
+            lf = 1.0;
+            for (int j = 0; j < n; j++)
+            {
+                if (i != j)
+                    lf = lf * (xp - x[j]) / (x[i] - x[j]);
+            }
+            sum += lf * f[i];
         }
-        printf("\nThe interpolated function value at x = %.5f is %.5f.", xp, fp);
-        printf("\nDo you want to contitnue? (y/n): ");
+        fp = sum;
+
+        printf("\nInterpolated function value at x = %.5f is %.5f.\n", xp, fp);
+        printf("Do you want to input another value? (y/n): ");
+        fflush(stdin); // clear input buffer
         scanf("%c", &q);
     } while (q == 'y');
     return 0;
