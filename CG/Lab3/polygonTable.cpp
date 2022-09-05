fdb7c94d9eace38b33c71677302387969f33dc9a
Suraj Pulami <54809836+surajmgr@users.noreply.github.com>
2022-09-05 11:17:44 +0545

    cg#lab3

---
+++ CG/Lab3/polygonTable.cpp
@@ -0,0 +1,32 @@
+#include <graphics.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main(){
+	int ver, face;
+	
+	int gdriver= DETECT, gmode;
+	initgraph(&gdriver, &gmode, "D:\\TURBOC3\\BGI");
+	
+	printf("Enter the number of faces: ");
+	scanf("%d", &face);
+	
+	do{
+		printf("Enter the number of vertices in face %d: ", face);
+		scanf("%d", &ver);
+		int poly[ver];
+		
+		for(int i=0; i<ver*2; i+2){
+			printf("Enter vertex from face %d (x,y): ", face);
+			scanf("%d %d", &poly[i], &poly[i+1]);
+		}
+		face--;
+	}while(face>1);
+	
+	drawpoly(ver, poly);
+	getch();
+
+	closegraph();
+	return 0;
+}
