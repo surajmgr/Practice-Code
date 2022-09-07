54b975b432ce169a30a5b2da52faaba1b6047a5c
Suraj Pulami <54809836+surajmgr@users.noreply.github.com>
2022-09-07 07:50:57 +0545

    dsa#linklist

---
+++ DSA/9_stackImplementationLinkedlist.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <stdlib.h>
+#include <conio.h>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node *next;
+};
+Node *top = NULL;
+
+void push(int num){
+	Node *new_node = (Node *)malloc(sizeof(Node));
+	new_node->data = num;
+	new_node->next = top;
+	top = new_node;
+}
+
+void pop(){
+	if(top == NULL)
+		cout<<"The list is empty!"<<endl;
+	else{
+		Node* temp = top;
+		top = temp->next;
+		free(temp);
+	}
+}
+
+void traversal(){
+    if (top == NULL)
+        cout << "The List is empty!!!" << endl;
+    else{
+        Node* temp = top;
+        while (temp != NULL)
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    int caseN;
+    char q;
+
+    do{
+        cout << "1. Push" << endl
+        	 << "2. Pop" <<endl
+             << "3. Display" << endl;
+        cin >> caseN;
+        switch (caseN){
+            case 1:
+                int n;
+                cout << "Enter the number to be inserted: ";
+                cin >> n;
+                push(n);
+                break;
+            case 2:
+            	pop();
+            	break;
+            case 3:	
+                traversal();
+                break;
+            default:
+                cout << "Invalid Choice!!!" << endl;
+                break;
+        }
+        cout << "Do you want to continue? (y/n): ";
+        cin >> q;
+    } while (q == 'y');
+
+    getch();
+    return 0;
+}
