a31bee6c71af5541a2e1fde3e3385498f8944bef
Suraj Pulami <54809836+surajmgr@users.noreply.github.com>
2022-11-08 10:32:49 +0545

    Create heunsMethod.cpp

---
+++ NM/Lab5/heunsMethod.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+
+float f(float x, float y) {
+  return y - x * x + 1;
+}
+
+int main() {
+  int i, n;
+  float x, y, xp, h, m1, m2;
+  printf("\nInput initial values of x and y: ");
+  scanf("%f%f", & x, & y);
+  printf("\nInput x at which y is required: ");
+  sceanf("%f", & xp);
+  printf("\nInput step-size h: ");
+  scanf("%f", & h);
+  n = (int)((xp - x) / h + 0.5);
+  for (i = 1; i <= n; i++) {
+    m1 = f(x, y);
+    m2 = f(x + h, y + m1 * h);
+    x = x + h;
+    y = y + 0.5 * h * (m1 + m2);
+    printf("\n%d\t %f\t %f\n", i, x, y);
+  }
+  printf("\nValue of y at x = %f is %f\n", xp, y);
+  getch();
+  return 0;
+}
